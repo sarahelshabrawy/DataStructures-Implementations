d280bca2202b2d376679d88f07a416de667da4ba
sarahelshabrawy <sarahelshabrawy@users.noreply.github.com>
2020-03-20 21:17:47 +0200

    Final

--- src/eg/edu/alexu/csd/datastructure/linkedList/cs24_cs28/PolynomialSolver.java
+++ src/eg/edu/alexu/csd/datastructure/linkedList/cs24_cs28/PolynomialSolver.java
@@ -10,8 +10,9 @@ public class PolynomialSolver implements IPolynomialSolver {
     SinglyLinkedList var2 ;
     @Override
     public void setPolynomial(char poly, int[][] terms) {
+
        if (Character.toUpperCase(poly) == 'A')
-            A = arraytolist(terms);
+           A = arraytolist(terms);
         else if (Character.toUpperCase(poly) == 'B')
             B = arraytolist(terms);
         else if (Character.toUpperCase(poly) == 'C') // return error on else ?
@@ -21,22 +22,24 @@ public class PolynomialSolver implements IPolynomialSolver {
     @Override
     public String print(char poly) {
         String polynomial = "";
-        var1 = sort(chooseList(poly));
-            for (int i = 0; i < var1.size(); i += 2) {
-                if ((int) var1.get(i) > 0) {
+        var1 = sort(EliminateZeroAndRepeated(chooseList(poly)));
+            for (int i = 0; i < this.var1.size(); i += 2) {
+                if ((int) this.var1.get(i) > 0) {
                     if (i != 0)
                         polynomial = polynomial.concat("+"); // to prevent +X-1
-                    if ((int) var1.get(i) != 1)
-                        polynomial = polynomial.concat(var1.get(i).toString()); // to prevent // 1X - 1
-                } else if ((int) var1.get(i) < 0)
-                    if ((int) var1.get(i) != -1)
-                        polynomial = polynomial.concat(var1.get(i).toString());
+                    if ((int) this.var1.get(i) != 1)
+                        polynomial = polynomial.concat(this.var1.get(i).toString()); // to prevent // 1X - 1
+                   if ((int) this.var1.get(i)==1 && (int) this.var1.get(i+1)==0)
+                    polynomial=polynomial.concat("1");
+                } else if ((int) this.var1.get(i) < 0)
+                    if ((int) this.var1.get(i) != -1)
+                        polynomial = polynomial.concat(this.var1.get(i).toString());
                     else
                         polynomial = polynomial.concat("-");
-                if ((int) var1.get(i) != 0 && (int)var1.get(i+1)!=0) {// to prevent X^0
+                if ((int) this.var1.get(i) != 0 && (int) this.var1.get(i+1)!=0) {// to prevent X^0
                     polynomial = polynomial.concat("X");
-                    if ((int)var1.get(i+1)!=1)
-                        polynomial = polynomial.concat("^" + var1.get(i + 1).toString()); // to prevent X^1
+                    if ((int) this.var1.get(i+1)!=1)
+                        polynomial = polynomial.concat("^" + this.var1.get(i + 1).toString()); // to prevent X^1
                 }
             }
             if(polynomial.length()==0)
@@ -52,7 +55,7 @@ public class PolynomialSolver implements IPolynomialSolver {
     @Override
     public float evaluatePolynomial(char poly, float value) {
         float result = 0;
-        var1 = chooseList(poly);
+            var1 = chooseList(poly);
             for (int i = 0; i < var1.size(); i += 2) {
                 result += (int) var1.get(i) * Math.pow(value, (int) (var1.get(i + 1)));
             }
@@ -90,7 +93,7 @@ public class PolynomialSolver implements IPolynomialSolver {
                 R.add(var2.get(i));
             }
         }
-        return listtoarray(R);
+        return listtoarray(sort(EliminateZeroAndRepeated(R)));
     }
 
     @Override
@@ -98,7 +101,6 @@ public class PolynomialSolver implements IPolynomialSolver {
         R.clear();
         var1 = chooseList(poly1);
         var2 = chooseList(poly2);
-      //  if((Character.toUpperCase(poly1) == 'A' && Character.toUpperCase(poly2) == 'B') || (Character.toUpperCase(poly1) == 'B' && Character.toUpperCase(poly2) == 'A')) {
             int flag ;
             for(int i=1 ; i<var1.size(); i+=2) {
                 flag =0 ;
@@ -123,10 +125,10 @@ public class PolynomialSolver implements IPolynomialSolver {
                 }
                 if(flag == 0) {
                     R.add(-(int)var2.get(i-1));
-                    R.add(B.get(i));
+                    R.add(var2.get(i));
                 }
             }
-        return listtoarray(R);
+        return listtoarray(sort(EliminateZeroAndRepeated(R)));
     }
 
     @Override
@@ -140,16 +142,7 @@ public class PolynomialSolver implements IPolynomialSolver {
                     R.add((int)var1.get(i+1) + (int)var2.get(j+1));
                 }
             }
-        for(int i=1 ; i<R.size();i+=2) {             // to eliminate terms with same exponent
-            for(int j =i+2 ; j<R.size() ; j+=2) {
-                if(R.get(i)== R.get(j)) {
-                    R.set(i-1,(int) R.get(i-1)+(int)R.get(j-1));
-                    R.remove(j-1);
-                    R.remove(j-1);
-                }
-            }
-        }
-        return listtoarray(R);
+        return listtoarray(sort(EliminateZeroAndRepeated(R)));
     }
 
     public SinglyLinkedList arraytolist (int[][] arr)
@@ -194,9 +187,34 @@ public class PolynomialSolver implements IPolynomialSolver {
             return A;
         else if (Character.toUpperCase(poly) == 'B')
             return B;
-        else if (Character.toUpperCase(poly) == 'C') // return error on else ?
+        else if (Character.toUpperCase(poly) == 'C')
             return C;
+        else if (Character.toUpperCase(poly) == 'R') // return error on else ?
+            return R;
         return null;
     }
-
+    public SinglyLinkedList EliminateZeroAndRepeated (SinglyLinkedList R){
+        for(int i=1 ; i<R.size();i+=2) {             // to eliminate terms with same exponent
+            for(int j =i+2 ; j<R.size() ;) {
+                if(R.get(i)== R.get(j)) {
+                    R.set(i-1,(int) R.get(i-1)+(int)R.get(j-1));
+                    R.remove(j-1);
+                    R.remove(j-1);
+                }
+                else j+=2;
+            }
+        }
+        for(int i = 0;i<R.size();) {            // to remove terms with zero coefficient
+            if((int)R.get(i)==0) {
+                R.remove(i);
+                R.remove(i);
+            }
+            else i+=2;
+        }
+        if(R.size() == 0){
+            R.add(0);
+            R.add(0);
+        }
+        return R;
+    }
 }
